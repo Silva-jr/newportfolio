2a68dbcb75e30fc16bba7d6273cec50c38566c8a
silva-jr <josedasilva0112@gmail.com>
2022-08-27 11:38:33 +0100

    first commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgsRevealConfig } from 'ngx-scrollreveal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'new-portfolio';
+  title = 'José da Silva';
+  constructor(config: NgsRevealConfig) {
+    
+
+  }
+
+  ngOnInit(): void {
+    const toggle_theme = document.querySelector('.toggle-theme');
+    const chance_theme = document.querySelector('.change-theme');
+
+    toggle_theme.addEventListener('click', () => {
+      document.documentElement.classList.toggle('dark');
+    });
+  }
 }
